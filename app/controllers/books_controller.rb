5b3ee37fcb04aa982a43381c7320108891cf65e6
Ramsey2022 <abovethelotus@yahoo.com>
2023-10-08 14:22:21 -0700

    added sorting logic using the order method

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,14 @@
 class BooksController < ApplicationController
 
   def index 
-    @books = Book.all
+    if params[:order] == 'author'
+      @books = Book.all.order('author')
+    elsif params[:order] == 'title'
+      @books = Book.all.order('title')
+    elsif params[:order] == 'year'
+      @books = Book.all.order('year')
+    else
+      @books = Book.all
+    end
   end
 end
